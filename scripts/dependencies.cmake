b2edead7b695ecf595e794077db4b73bf18b0baa
Alilux <aliluxxxxx@gmail.com>
2026-01-25 11:47:02 +0100

    Added dependencies.cmake file

---
+++ scripts/dependencies.cmake
@@ -0,0 +1,35 @@
+# Include dirs
+set(SANDBOX_INCLUDE_DIRS
+	${CMAKE_CURRENT_SOURCE_DIR}/src
+	${CMAKE_SOURCE_DIR}/sandbox-core/src
+)
+
+# Library dirs
+set(SANDBOX_LIB_DIRS
+	"sandbox-core"
+)
+
+# Vulkan dependencies
+find_package(Vulkan REQUIRED)
+message(STATUS "Vulkan include dirs: ${Vulkan_INCLUDE_DIRS}")
+message(STATUS "Vulkan libraries: ${Vulkan_LIBRARIES}")
+
+set(VULKAN_INCLUDE_DIR ${Vulkan_INCLUDE_DIRS})
+list(APPEND SANDBOX_INCLUDE_DIRS ${VULKAN_INCLUDE_DIR})
+get_filename_component(VULKAN_LIB_DIR ${Vulkan_LIBRARIES} DIRECTORY)
+list(APPEND SANDBOX_LIB_DIRS "${VULKAN_LIB_DIR}/vulkan-1.lib")
+
+set(VULKAN_LIB_DIRS_DEBUG
+	"${VULKAN_LIB_DIR}/shaderc_sharedd.lib"
+	"${VULKAN_LIB_DIR}/spirv-cross-cored.lib"
+	"${VULKAN_LIB_DIR}/spirv-cross-glsld.lib"
+	"${VULKAN_LIB_DIR}/SPIRV-Toolsd.lib"
+)
+
+set(VULKAN_LIB_DIRS_RELEASE
+	"${VULKAN_LIB_DIR}/vulkan-1.lib"
+	"${VULKAN_LIB_DIR}/shaderc_shared.lib"
+	"${VULKAN_LIB_DIR}/spirv-cross-core.lib"
+	"${VULKAN_LIB_DIR}/spirv-cross-glsl.lib"
+	"${VULKAN_LIB_DIR}/SPIRV-Tools.lib"
+)
